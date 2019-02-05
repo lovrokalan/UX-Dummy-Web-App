bd9cbafc465f73a8b5fd86bee7b96eea723c57f7
Lovro Kalan <lovro.kalan1@gmail.com>
2019-02-05 21:29:15 +0100

    Fixed menu animations, responsivness,.. Main content added.

--- dom.js
+++ dom.js
@@ -1,11 +1,11 @@
 var slideMenuMinified = true;
 var menuElements = document.getElementsByClassName('menuBtn');
 
-function openSlideMenu() {
+function slideMenu() {
   
   if(slideMenuMinified){
     document.getElementById('side-menu').style.width = '250px';
-    document.getElementById('main').style.marginLeft = '250px';
+    document.getElementById('main').style.marginLeft = '240px';
     document.getElementsByClassName('btn-menu')[0].style.paddingLeft = '120px';
     document.getElementById('logo').style.marginLeft = '170px';
     document.getElementById('navArrow').src = "./svgs/arrow_left.svg";
@@ -16,12 +16,11 @@ function openSlideMenu() {
         menuElements[i].style.paddingLeft = '20px';
       }
     }, 200);
-
     slideMenuMinified = false;
-  }
-  else {
+
+  } else {
     document.getElementById('side-menu').style.width = '100px';
-    document.getElementById('main').style.marginLeft = '100px';
+    document.getElementById('main').style.marginLeft = '90px';
     document.getElementsByClassName('btn-menu')[0].style.paddingLeft = '0px';
     document.getElementById('logo').style.marginLeft = '20px';
     document.getElementById('navArrow').src = "./svgs/arrow_right.svg";
@@ -29,10 +28,31 @@ function openSlideMenu() {
       menuElements[i].classList.add('description-hidden');
       menuElements[i].style.paddingLeft = '0px';
     }
-
     slideMenuMinified = true;
   }
 }
+
+function slideToMobile(){
+  document.getElementById('side-menu').style.width = '50px';
+  document.getElementById('main').style.marginLeft = '40px';
+  document.getElementsByClassName('btn-menu')[0].style.paddingLeft = '0px';
+  document.getElementById('logo').style.marginLeft = '20px';
+  document.getElementById('navArrow').src = "./svgs/arrow_right.svg";
+  for (var i = 0; i < menuElements.length; i++) {
+    menuElements[i].classList.add('description-hidden');
+    menuElements[i].style.paddingLeft = '0px';
+  }
+  slideMenuMinified = true;
+}
+
+$(window).resize(function () {
+  if ($(window).width() < 700) {
+    slideToMobile();
+  } else {
+    slideMenuMinified = false;
+    slideMenu();
+  }
+});
 
 var dropdownUser = document.getElementById('drop-down');
 var usernameLogo = document.getElementById('userInfo');
@@ -47,4 +67,5 @@ function openDropdown() {
 
 function closeDropdown() {
   dropdownUser.style.display = 'none';
-} 
+}
+
